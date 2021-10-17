187e8895be99288b26ab49b47efae864d6664b1b
lenny-g <leanne_gallag@live.co.uk>
2021-10-17 23:56:03 +0100

    havent finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,87 +1,121 @@
 const timeData = [
   {
     name: "9am",
-    number: 9,
+    hour: 09,
   },
   {
     name: "10am",
-    number: 10,
+    hour: 10,
   },
   {
     name: "11am",
-    number: 11,
+    hour: 11,
   },
   {
     name: "12pm",
-    number: 12,
+    hour: 12,
   },
   {
     name: "1pm",
-    number: 1,
+    hour: 13,
   },
   {
     name: "2pm",
-    number: 2,
+    hour: 14,
   },
   {
     name: "3pm",
-    number: 3,
+    hour: 15,
   },
   {
     name: "4pm",
-    number: 4,
+    hour: 16,
   },
   {
     name: "5pm",
-    number: 5,
+    hour: 17,
   },
 ];
-const currentTime = moment().format("LTS");
-const dateTitle = moment().format("LLLL");
 
+const currentTime = moment().format("HH");
+console.log(this);
+
+// The date of the current day
+const dateTitle = moment().format("dddd, MMMM Do");
+
+// constructing the display of the current da
 const constructDateTitle = function () {
   document.getElementById("currentDay").textContent = dateTitle;
 };
 
-const getCurrent = function () {
-  // check current time and compare with timeBlock
-  // jQuery each loop
-  // if else function
-  // set class of future, past and present
-};
-
 const constructTimeBlock = function () {
   const callback = function (each) {
-    const timeBlockHour = `<div class="time-block">
-    <div class="hour">${each.name}</div>
-    <div class="input">
-      <textarea placeholder="Please enter text..."></textarea>
-    </div>
-    <div class="save">
-      <button class="saveBtn">SAVE</button>
-    </div>
-    </div>`;
-
-    $(".container").append(timeBlockHour);
-    return timeBlockHour;
+    const timeBlock = `
+    <div class="time-block">
+    <form class ="row">
+    <div class="hour column">${each.name}</div>
+    <div class="input column">
+    <textarea class="text-input" placeholder="Please enter text..."></textarea>
+     </div>
+     <div class="save column">
+     <button class="saveBtn">SAVE</button>
+   </div>
+    </form>
+    </div>"`;
+    $(".container").append(timeBlock);
+    return timeBlock;
   };
 
   timeData.forEach(callback);
 };
 
-const initialiseLocalStorage = function () {
-  // steal code!
+const verifyInput = function (event) {
+  const target = event.target;
+  const currentTarget = event.currentTarget;
+
+  if (target.getAttribute("class") === "saveBtn") {
+    saveInput();
+  } else {
+    console.log("NO!");
+  }
+};
+
+const getFromLocalStorage = function (key, defaultValue) {
+  const localStorageData = JSON.parse(localStorage.getItem(key));
+
+  if (!localStorageData) {
+    return defaultValue;
+  } else {
+    return localStorageData;
+  }
+};
+
+const findTense = function (each) {
+  const textInput = document.querySelector(".text-input");
+  const listTime = timeData.hour;
+  console.log(listTime);
+
+  if (listTime < currentTime) {
+    textInput.setAttribute("class", "past");
+  } else if (listTime === currentTime) {
+    textInput.setAttribute("class", "present");
+  } else if (listTime > currentTime) {
+    textInput.setAttribute("class", "future");
+  } else {
+    timeData++;
+  }
+  listTime.forEach(findTense);
 };
 
 const saveInput = function (event) {
-  // target where user has clicked
-  // only runs when clicked on button (event.target)
-  // check local storage (get from)
-  // retrieve user input
-  // what hour in timeblock
-  // hour:input
-  // add to local storage
-  // restore
+  localStorage.setItem("name", JSON.stringify([]));
+};
+
+const initialiseLocalStorage = function () {
+  const dataFromLS = JSON.parse(localStorage.getItem("name"));
+  if (!dataFromLS) {
+    localStorage.setItem("name", JSON.stringify([]));
+  }
 };
 
 const onLoad = function () {
@@ -90,5 +124,5 @@ const onLoad = function () {
   initialiseLocalStorage();
 };
 
+$(".container").click(verifyInput);
 $(document).ready(onLoad);
-$(".container").click(saveInput);
